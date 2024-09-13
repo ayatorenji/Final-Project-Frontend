ff80f285e786362958e71699ae5f6463f532be4c
Chayanon <koonjjkung.castgame@gmail.com>
2024-09-13 15:54:29 +0700

    Uodate axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,8 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const RESTURL = "http://localhost:3000/api"; // need to create backend first
+// const RESTURL = "http://localhost:3000/api"; // need to create backend first
+const RESTURL = "https://final-project-backend-iv58.onrender.com/api"; // need to create backend first
 const api = axios.create({
   baseURL: RESTURL,
   headers: { "Content-type": "application/json" },
