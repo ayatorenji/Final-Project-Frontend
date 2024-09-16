d1ae107df2a1c63bf955d9559b3e5265806c1fee
Chayanon <koonjjkung.castgame@gmail.com>
2024-09-16 13:13:14 +0700

    Update routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,23 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/', component: () => import('pages/IndexPage.vue') },
+    ]
+  },
+
+  {
+    path: '/main',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
       { path: '/register', component: () => import('pages/RegisterPage.vue') },
       { path: '/login', component: () => import('pages/LoginPage.vue') },
+    ]
+  },
+
+  {
+    path: '/content',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
       { path: '/post', component: () => import('pages/PostPage.vue') },
       { path: '/dashboard', component: () => import('pages/DashboardPage.vue') },
       { path: '/pet', component: () => import('pages/AnimalPage.vue') },
